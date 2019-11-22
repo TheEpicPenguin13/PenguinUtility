7ea226ba8135cc4fa18f0bd3efe2c1c86ec6d6b3
John McAllister <theepicpenguin121@gmail.com>
2019-11-21 18:58:53 -0800

    Minor Edits, Need To Change Logging Bug

--- src/discord-bot/commands/general/help.js
+++ src/discord-bot/commands/general/help.js
@@ -1,13 +1,20 @@
 const Discord = require("discord.js");
 
 module.exports.run = (client, message, args) => {
+    const commandName = args[0];
     switch(args.length) {
         case 0:
             break;
         case 1:
+            const cmd = client.commands.get(commandName);
+            if (!cmd) return;
             const embed = new Discord.RichEmbed()
-            .setTitle(`Help For The Command ${args[0]}`)
-            .setDescription(``);
+            .setTitle(`Help For The Command ${cmd.config.name || "No Name Given"}`)
+            .setDescription(`${cmd.config.description || "No Description Given"}`)
+            .addField("Aliases", `${cmd.config.aliases.length > 0 ? cmd.config.aliases : "No Aliases Given"}`)
+            .addField("Permission Level", `${cmd.config.permissionLevel || "No Permission Level Specified"}`)
+            .addField("Category", `${cmd.config.category || "No Category Specified"}`);
+            message.channel.send({embed});
             break;
         default:
             message.reply("You have supplied too many commands.");
