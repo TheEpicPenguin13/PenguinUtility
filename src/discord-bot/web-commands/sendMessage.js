d58306377e07d9b228b1f66f1d0821cd1464dbfa
TheEpicPenguin13 <TheEpicPenguin13@users.noreply.github.com>
2019-11-21 21:06:44 -0800

    Oops

--- src/discord-bot/web-commands/sendMessage.js
+++ src/discord-bot/web-commands/sendMessage.js
@@ -1,7 +1,9 @@
+const Logger = require('../../logger');
+
 module.exports = (client, channelName, message) => {
     const channel = client.channels.find(channel => channel.name === channelName);
     if (!channel) return;
     client.channels.get(channel.id).send(message);
-    console.log(`[Web Command Logs] Sent "${message}" Via ${client.config.system.website} In #${channelName}`.debug);
+    Logger.log(`[Web Command Logs] Sent "${message}" Via ${client.config.system.website} In #${channelName}`)
     return;
 }
